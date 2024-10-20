654955be3c58bab7dc822a4763c499bab180dfdf
Caio Ribeiro de Carvalho <71287644+CaioCarvalho314@users.noreply.github.com>
2024-10-19 21:14:01 -0300

    feat: add damageSystem

--- UNESP_CPM_GRAF/Assets/AmbulanceHealth.cs
+++ UNESP_CPM_GRAF/Assets/AmbulanceHealth.cs
@@ -1,36 +1,92 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.VersionControl;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AmbulanceHealth : MonoBehaviour
 {
+    private float maxHealth;
+    private float animationDuration = 0.25f;
+    private float maxDamage = 15f;
+    private float damageTime = 0f;
+    private float currentSpeed = 0f;
+
     public Image healthBar;
-    public float healthValue = 0f;
+    public float healthValue = 50f;
+
     public Collider ambulanceColider;
+    public Rigidbody ambulanceRigidbody;
+
+    public AudioClip collisionSound; // Som de colisão
+    public AudioClip stopSound; // Som específico para a tag "Stop"
+    public AudioSource audioSource; // Fonte de áudio
+
+    private AmbulanceLights ambulanceLights;
 
     void Start()
     {
-        
+        ambulanceLights = GetComponent<AmbulanceLights>();
+        maxHealth = healthValue;
     }
 
     void Update()
     {
+        currentSpeed = ambulanceRigidbody.velocity.magnitude * 3.6f; // Converte para km/h
     }
-
 
     void OnTriggerEnter(Collider other)
     {
-            healthValue -= 10f; 
-            HealthChange(healthValue);
+        float damageTaken;
+        float newHealthValue;
+
+        // Colisão regular (não é "Stop")
+        if (!other.CompareTag("Stop"))
+        {
+            damageTaken = (1f * currentSpeed) > maxDamage ? maxDamage : (0.5f * currentSpeed);
+            newHealthValue = Mathf.Clamp(healthValue - damageTaken, 0f, maxHealth);
+
+            // Se passar 1 segundo desde o último dano
+            if (Time.time - damageTime > 1f)
+            {
+                StartCoroutine(HealthChange(healthValue, newHealthValue));
+                healthValue = newHealthValue;
+                damageTime = Time.time;
+                audioSource.PlayOneShot(collisionSound);
+            }
+        }
+        // Colisão com a tag "Stop" e sirene desligada
+        else if (other.CompareTag("Stop") && !ambulanceLights.IsSirenActive())
+        {
+            damageTaken = 10f;
+            newHealthValue = Mathf.Clamp(healthValue - damageTaken, 0f, maxHealth);
+
+            if (Time.time - damageTime > 1f)
+            {
+                StartCoroutine(HealthChange(healthValue, newHealthValue));
+                healthValue = newHealthValue;
+                damageTime = Time.time;      
+                audioSource.PlayOneShot(stopSound);
+                
+            }
+        }
     }
 
-    void HealthChange(float healthValue)
+    IEnumerator HealthChange(float oldHealth, float newHealth)
     {
-        float healthAmount = (healthValue / 100.0f) * 180.0f / 360;
-        healthBar.fillAmount = healthAmount;
+        float elapsedTime = 0f;
+        while (elapsedTime < animationDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float currentHealth = Mathf.Lerp(oldHealth, newHealth, elapsedTime / animationDuration);
+            UpdateHealthBar(currentHealth);
+            yield return null;
+        }
+        UpdateHealthBar(newHealth);
+    }
 
+    void UpdateHealthBar(float health)
+    {
+        float healthAmount = (health / maxHealth) * 180.0f / 360;
+        healthBar.fillAmount = healthAmount;
     }
 }
